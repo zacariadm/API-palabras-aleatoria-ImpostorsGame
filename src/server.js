04d103937857eca752ebb3d0fce78a3ea9fcf228
Carlos Archidona <carlos.archidona@educa.madrid.org>
2026-02-23 09:28:30 +0100

    Arreglar fallo de conexion con la API

--- src/server.js
+++ src/server.js
@@ -1,16 +1,35 @@
-const express = require('express')
-const wordRoute = require('./routes/wordsRoute.js')
-const wordAnimalsRoute = require('./routes/wordsAnimalsRoute.js')
-const wordObjectsRoute = require('./routes/wordsObjectsRoute.js')
-const categoriesRoute = require('./routes/categoriesRoute');
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-//middlewares
-app.use('/api', wordRoute)
-app.use('/api', wordAnimalsRoute)
-app.use('/api', wordObjectsRoute)
+const wordsRoute = require('./routes/wordsRoute.js');
+const wordsAnimalesRoute = require('./routes/wordsAnimalesRoute.js');
+const wordsComponentesOrdenadorRoute = require('./routes/wordsComponentesOrdenadorRoute.js');
+const wordsComunidadesAutonomasEspanaRoute = require('./routes/wordsComunidadesAutonomasEspana.js');
+const wordsDeportesRoute = require('./routes/wordsDeportesRoute.js');
+const wordsGeneralRoute = require('./routes/wordsGeneralRoute.js');
+const wordsJuegosRoute = require('./routes/wordsJuegosRoute.js');
+const wordsMarcasCocheRoute = require('./routes/wordsMarcasCocheRoute.js');
+const wordsMarcasRelojesRoute = require('./routes/wordsMarcasRelojesRoute.js');
+const wordsObjetoComunesRoute = require('./routes/wordsObjetoComunesRoute.js');
+const wordsPaisesRoute = require('./routes/wordsPaisesRoute.js');
+const wordsPrendasRopaRoute = require('./routes/wordsPrendasRopaRoute.js');
+const wordsTiposEdificioRoute = require('./routes/wordsTiposEdificioRoute.js');
+const categoriesRoute = require('./routes/categoriesRoute.js');
+
+app.use('/api', wordsRoute);
+app.use('/api', wordsAnimalesRoute);
+app.use('/api', wordsComponentesOrdenadorRoute);
+app.use('/api', wordsComunidadesAutonomasEspanaRoute);
+app.use('/api', wordsDeportesRoute);
+app.use('/api', wordsGeneralRoute);
+app.use('/api', wordsJuegosRoute);
+app.use('/api', wordsMarcasCocheRoute);
+app.use('/api', wordsMarcasRelojesRoute);
+app.use('/api', wordsObjetoComunesRoute);
+app.use('/api', wordsPaisesRoute);
+app.use('/api', wordsPrendasRopaRoute);
+app.use('/api', wordsTiposEdificioRoute);
 app.use('/', categoriesRoute);
 
-// listen
-app.listen(port, () => console.log('Server running in port '+ port))
+app.listen(port, () => console.log('Server running in port ' + port));
